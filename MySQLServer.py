897fd8afdfc00bea87c72285b731a9a2622acbcc
thisissernamalala <222650494@mycput.ac.za>
2024-10-19 21:51:11 +0200

    Added some exception handling code

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,18 +1,29 @@
 import mysql.connector
+from mysql.connector import Error
 
+try:
 # Replace with your connection details
-mydb = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Serna@2003",
-    database="serna"
-)
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Serna@2003",
+        database="serna"
+    )
 
-print(mydb.get_server_info())
+    print(mydb.get_server_info())
 
-if mydb.is_connected():
-    mycursor = mydb.cursor()
-    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
+    if mydb.is_connected():
+        mycursor = mydb.cursor()
+        mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
+except Error as e:
+    print(f"Error connecting to MySQL: {e}")
+
+finally:
+        # Ensure the connection is closed
+    if mydb.is_connected():
+        mycursor.close()
+        mydb.close()
+        print("MySQL connection closed.")
 
