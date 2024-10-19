23854c32327af46b2dbd3d620d168233fafdfdee
thisissernamalala <222650494@mycput.ac.za>
2024-10-19 21:33:41 +0200

    created the mysql file and created the database with tables

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -34,6 +34,6 @@ CREATE TABLE Order_Details(
     order_id INT FOREIGN KEY,
     book_id INT FOREIGN KEY,
     quantity DOUBLE,
-    FOREIGN KEY (order_id) REFERENCES Orders (order_id),
-    FOREIGN KEY (book_id) REFERENCES Books (book_id_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id_id)
 )
